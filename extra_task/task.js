e4f868efaaad0230131b1894121dad66413a0ce9
Denis Oleschenko <denisoleshenko@gmail.com>
2019-10-05 19:47:31 +0300

    extra task solution

---
+++ extra_task/task.js
@@ -0,0 +1,49 @@
+const http = require('http')
+class App {
+	constructor() {
+		this.nextStack = []
+	}
+
+	use(fn) {
+		this.nextStack.push(fn)
+	}
+
+	handle(req, res) {
+		this.nextStack = this.nextStack.map((fn, index) => {
+			return () => fn(req, res, () => this.nextStack[index + 1](req, res))
+		})
+		this.nextStack[0]()
+	}
+}
+
+const app = new App()
+const server = http.createServer((req, res) => app.handle(req, res))
+
+app.use((req, res, next) => {
+	console.log('middleware 1 start')
+	req.test = 'hello from middleware 1'
+	next()
+	console.log('middleware 1 end')
+})
+
+app.use((req, res, next) => {
+	console.log('middleware 2')
+	console.log(req.test)
+	next()
+})
+
+app.use((req, res, next) => {
+	console.log('middleware 3')
+	res.end('Hello')
+})
+
+server.listen(3000, () => {
+	console.log('listening...')
+})
+
+// Output in the console should be
+// middleware 1 start
+// middleware 2
+// hello from middleware 1
+// middleware 3
+// middleware 1 end
