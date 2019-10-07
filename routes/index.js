d2a61958a8d2d8a1a0dec9d3e6dcee1174c92b63
Denis Oleschenko <denisoleshenko@gmail.com>
2019-10-07 15:08:56 +0300

    the odd router was deleted

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 const https = require('https')
-const mainRouter = require('express').Router()
 const router = require('express').Router()
 
 const blog = require('./blogs')
 const user = require('./users')
 
-mainRouter.get('*', (req, res) => {
-	https.get(process.env.FRONTEND_URL, response => response.pipe(res))
-})
-
 router.use('/api/v1/users', user)
 router.use('/api/v1/blog', blog)
-router.use('/', mainRouter)
+
+router.get('*', (req, res) => {
+	https.get(process.env.FRONTEND_URL, response => response.pipe(res))
+})
 
 module.exports = router
