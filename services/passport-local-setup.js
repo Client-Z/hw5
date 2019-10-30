859a8e68cc3cab28eeb721177011b323281c956a
Denis Oleschenko <denisoleshenko@gmail.com>
2019-10-30 16:35:02 +0200

    test refactor 1

--- services/passport-local-setup.js
+++ services/passport-local-setup.js
@@ -6,49 +6,12 @@ const { errorLogger } = require('./logger')
 const { checkPsw } = require('./hashPsw')
 const { Users, Providers } = require('../db/models/index.js')
 
-const createOauthAccount = async (userID, profile) => {
-	await Providers.create({ userId: userID, provider: profile.provider, providerUserId: profile.id })
+const createOauthAccount = async (userID, provider, providerUserId) => {
+	await Providers.create({ userId: userID, provider: provider, providerUserId: providerUserId })
 }
 
-const createUser = async profile => {
-	let firstName = ''
-	let lastName = ''
-	let email = profile.emails[0].value
-	if (!profile.name.givenName || !profile.name.familyName) {
-		const splitPoint = profile.displayName.indexOf(' ')
-		if (splitPoint) {
-			firstName = profile.displayName.slice(0, splitPoint)
-			lastName = profile.displayName.slice(splitPoint)
-		} else {
-			firstName = profile.displayName
-			lastName = 'Is not indicated'
-		}
-	} else {
-		firstName = profile.name.givenName
-		lastName = profile.name.familyName
-	}
+const createUser = async (firstName, lastName, email) => {
 	return await Users.create({ firstName: firstName, lastName: lastName, email: email, password: '' })
-}
-
-const oauthStrategy = async (profile, done) => {
-	if (profile.id) {
-		try {
-			// Check if we already have a user with this email
-			const userData = await Users.findOne({ where: { email: profile.emails[0].value } })
-			if (userData) {
-				const user = userData.dataValues
-				const hasAccount = await Providers.findOne({ where: { providerUserId: profile.id } })
-				if (!hasAccount) createOauthAccount(user.id, profile)
-				done(null, user)
-			} else {
-				const newUser = await createUser(profile)
-				createOauthAccount(newUser.dataValues.id, profile)
-				done(null, newUser.dataValues)
-			}
-		} catch (err) {
-			errorLogger.error(`An error on MySQL request`, { metadata: err })
-		}
-	}
 }
 
 module.exports = passport => {
@@ -56,10 +19,8 @@ module.exports = passport => {
 
 	passport.deserializeUser(async (id, done) => {
 		try {
-			const userData = await Users.findAll({ where: { id: id } })
-			const user = Users.build(userData[0].dataValues)
-			if (user) return done(null, user)
-			done(null, false)
+			const user = await Users.findByPk(id)
+			return done(null, user)
 		} catch (err) {
 			done(err)
 			errorLogger.error(`An error on MySQL request`, { metadata: err })
@@ -88,7 +49,27 @@ module.exports = passport => {
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET
 			},
-			(accessToken, refreshToken, profile, done) => oauthStrategy(profile, done)
+			async (accessToken, refreshToken, profile, done) => {
+				if (profile.id) {
+					try {
+						// Check if we already have a user with this email
+						const email = profile.emails[0].value
+						const userData = await Users.findOne({ where: { email: email } })
+						if (userData) {
+							const user = userData.dataValues
+							const hasAccount = await Providers.findOne({ where: { providerUserId: profile.id } })
+							if (!hasAccount) createOauthAccount(user.dataValues.id, profile.provider, profile.id)
+							done(null, user)
+						} else {
+							const newUser = await createUser(profile.name.givenName, profile.name.familyName, email)
+							createOauthAccount(newUser.dataValues.id, profile.provider, profile.id)
+							done(null, newUser.dataValues)
+						}
+					} catch (err) {
+						errorLogger.error(`An error on MySQL request`, { metadata: err })
+					}
+				}
+			}
 		)
 	)
 
@@ -100,7 +81,42 @@ module.exports = passport => {
 				callbackURL: process.env.FACEBOOK_CALLBACK_URL,
 				profileFields: ['id', 'displayName', 'email']
 			},
-			(accessToken, refreshToken, profile, done) => oauthStrategy(profile, done)
+			async (accessToken, refreshToken, profile, done) => {
+				if (profile.id) {
+					try {
+						// Check if we already have a user with this email
+						const userData = await Users.findOne({ where: { email: profile.emails[0].value } })
+						if (userData) {
+							const user = userData.dataValues
+							const hasAccount = await Providers.findOne({ where: { providerUserId: profile.id } })
+							if (!hasAccount) createOauthAccount(user.dataValues.id, profile.provider, profile.id)
+							done(null, user)
+						} else {
+							let firstName = ''
+							let lastName = ''
+							let email = profile.emails[0].value
+							if (!profile.name.givenName || !profile.name.familyName) {
+								const splitPoint = profile.displayName.indexOf(' ')
+								if (splitPoint) {
+									firstName = profile.displayName.slice(0, splitPoint)
+									lastName = profile.displayName.slice(splitPoint)
+								} else {
+									firstName = profile.displayName
+									lastName = 'Is not indicated'
+								}
+							} else {
+								firstName = profile.name.givenName
+								lastName = profile.name.familyName
+							}
+							const newUser = await createUser(firstName, lastName, email)
+							createOauthAccount(newUser.dataValues.id, profile.provider, profile.id)
+							done(null, newUser.dataValues)
+						}
+					} catch (err) {
+						errorLogger.error(`An error on MySQL request`, { metadata: err })
+					}
+				}
+			}
 		)
 	)
 }
