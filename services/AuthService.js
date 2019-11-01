108563e2fe7bfdd7e8dc1cd23d434024d9246251
Denis Oleschenko <denisoleshenko@gmail.com>
2019-11-01 18:21:08 +0200

    commit for test oauth through fb

---
+++ services/AuthService.js
@@ -0,0 +1,37 @@
+// 	provider: 'google',
+// 	email: profile._json.email,
+// 	firstName: profile._json.given_name,
+// 	lastName: profile._json.family_name,
+// 	providerUserId: profile.id
+const { errorLogger } = require('./logger')
+const { Users, Providers } = require('../db/models/index.js')
+
+class AuthService {
+	static async findOrCreateOauthProvider(data) {
+		try {
+			const userData = await Users.findOne({ where: { email: data.email } })
+			if (userData) {
+				const user = userData.get({ plain: true })
+				const hasAccount = await Providers.findOne({ where: { providerUserId: data.providerUserId } })
+				if (!hasAccount) await this.createOauthAccount(user.id, data.provider, data.providerUserId)
+				return user
+			} else {
+				const newUser = await this.createUser(data.firstName, data.lastName, data.email)
+				await this.createOauthAccount(newUser.dataValues.id, data.provider, data.providerUserId)
+				return newUser.get({ plain: true })
+			}
+		} catch (err) {
+			errorLogger.error(`An error on MySQL request`, { metadata: err })
+		}
+	}
+
+	static async createOauthAccount(userID, provider, providerUserId) {
+		await Providers.create({ userId: userID, provider: provider, providerUserId: providerUserId })
+	}
+
+	static async createUser(firstName, lastName, email) {
+		return await Users.create({ firstName: firstName, lastName: lastName, email: email, password: '' })
+	}
+}
+
+module.exports = { AuthService }
--- services/passportService.js
+++ services/passportService.js
@@ -4,15 +4,8 @@ const FacebookStrategy = require('passport-facebook').Strategy
 
 const { errorLogger } = require('./logger')
 const { checkPsw } = require('./hashPsw')
-const { Users, Providers } = require('../db/models/index.js')
-
-const createOauthAccount = async (userID, provider, providerUserId) => {
-	await Providers.create({ userId: userID, provider: provider, providerUserId: providerUserId })
-}
-
-const createUser = async (firstName, lastName, email) => {
-	return await Users.create({ firstName: firstName, lastName: lastName, email: email, password: '' })
-}
+const { Users } = require('../db/models/index.js')
+const { AuthService } = require('./AuthService')
 
 module.exports = passport => {
 	passport.serializeUser((user, done) => done(null, user.id))
@@ -42,36 +35,25 @@ module.exports = passport => {
 		})
 	)
 
-	const checkUser = async (email, provider, providerID) => {
-		const userData = await Users.findOne({ where: { email: email } })
-		if (userData) {
-			const user = userData.get({ plain: true })
-			const hasAccount = await Providers.findOne({ where: { providerUserId: providerID } })
-			if (!hasAccount) await createOauthAccount(user.id, provider, providerID)
-			return user
-		}
-		return false
-	}
-
 	passport.use(
 		new GoogleStrategy(
 			{
-				callbackURL: process.env.GOOGLE_CALLBACK_URL,
 				clientID: process.env.GOOGLE_CLIENT_ID,
-				clientSecret: process.env.GOOGLE_CLIENT_SECRET
+				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+				callbackURL: process.env.GOOGLE_CALLBACK_URL
 			},
 			async (accessToken, refreshToken, profile, done) => {
-				if (profile.id) {
-					try {
-						const email = profile.emails[0].value
-						const user = await checkUser(email, profile.provider, profile.id)
-						if (user) return done(null, user)
-						const newUser = await createUser(profile.name.givenName, profile.name.familyName, email)
-						createOauthAccount(newUser.dataValues.id, profile.provider, profile.id)
-						done(null, newUser)
-					} catch (err) {
-						errorLogger.error(`An error on MySQL request`, { metadata: err })
-					}
+				try {
+					const user = await AuthService.findOrCreateOauthProvider({
+						provider: 'google',
+						email: profile._json.email,
+						firstName: profile._json.given_name,
+						lastName: profile._json.family_name,
+						providerUserId: profile.id
+					})
+					done(null, user)
+				} catch (e) {
+					done(e)
 				}
 			}
 		)
@@ -83,37 +65,77 @@ module.exports = passport => {
 				clientID: process.env.FACEBOOK_CLIENT_ID,
 				clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
 				callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-				profileFields: ['id', 'displayName', 'email']
+				profileFields: ['id', 'email', 'first_name', 'last_name']
 			},
 			async (accessToken, refreshToken, profile, done) => {
-				if (profile.id) {
-					try {
-						const email = profile.emails[0].value
-						const user = await checkUser(email, profile.provider, profile.id)
-						if (user) return done(null, user)
-						let firstName = ''
-						let lastName = ''
-						if (!profile.name.givenName || !profile.name.familyName) {
-							const splitPoint = profile.displayName.indexOf(' ')
-							if (splitPoint) {
-								firstName = profile.displayName.slice(0, splitPoint)
-								lastName = profile.displayName.slice(splitPoint)
-							} else {
-								firstName = profile.displayName
-								lastName = 'Is not indicated'
-							}
-						} else {
-							firstName = profile.name.givenName
-							lastName = profile.name.familyName
-						}
-						const newUser = await createUser(firstName, lastName, email)
-						createOauthAccount(newUser.dataValues.id, profile.provider, profile.id)
-						done(null, newUser)
-					} catch (err) {
-						errorLogger.error(`An error on MySQL request`, { metadata: err })
-					}
+				try {
+					// let firstName = ''
+					// let lastName = ''
+					// if (!profile.name.givenName || !profile.name.familyName) {
+					// 	const splitPoint = profile.displayName.indexOf(' ')
+					// 	if (splitPoint) {
+					// 		firstName = profile.displayName.slice(0, splitPoint)
+					// 		lastName = profile.displayName.slice(splitPoint)
+					// 	} else {
+					// 		firstName = profile.displayName
+					// 		lastName = 'Is not indicated'
+					// 	}
+					// } else {
+					// 	firstName = profile.name.givenName
+					// 	lastName = profile.name.familyName
+					// }
+					const user = await AuthService.findOrCreateOauthProvider({
+						provider: 'facebook',
+						email: profile._json.email,
+						firstName: profile.name.givenName,
+						lastName: profile.name.familyName,
+						providerUserId: profile.id
+					})
+					done(null, user)
+				} catch (error) {
+					done(error)
 				}
 			}
 		)
 	)
+
+	// 	passport.use(
+	// 		new FacebookStrategy(
+	// 			{
+	// 				clientID: process.env.FACEBOOK_CLIENT_ID,
+	// 				clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+	// 				callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+	// 				profileFields: ['id', 'displayName', 'email']
+	// 			},
+	// 			async (accessToken, refreshToken, profile, done) => {
+	// 				if (profile.id) {
+	// 					try {
+	// 						const email = profile.emails[0].value
+	// 						const user = await checkUser(email, profile.provider, profile.id)
+	// 						if (user) return done(null, user)
+	// 						let firstName = ''
+	// 						let lastName = ''
+	// 						if (!profile.name.givenName || !profile.name.familyName) {
+	// 							const splitPoint = profile.displayName.indexOf(' ')
+	// 							if (splitPoint) {
+	// 								firstName = profile.displayName.slice(0, splitPoint)
+	// 								lastName = profile.displayName.slice(splitPoint)
+	// 							} else {
+	// 								firstName = profile.displayName
+	// 								lastName = 'Is not indicated'
+	// 							}
+	// 						} else {
+	// 							firstName = profile.name.givenName
+	// 							lastName = profile.name.familyName
+	// 						}
+	// 						const newUser = await createUser(firstName, lastName, email)
+	// 						createOauthAccount(newUser.dataValues.id, profile.provider, profile.id)
+	// 						done(null, newUser)
+	// 					} catch (err) {
+	// 						errorLogger.error(`An error on MySQL request`, { metadata: err })
+	// 					}
+	// 				}
+	// 			}
+	// 		)
+	// 	)
 }
