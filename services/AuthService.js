96157e2fa5345143829e56306eb1936212760ad0
Denis Oleschenko <denisoleshenko@gmail.com>
2019-11-01 21:38:56 +0200

    little fixes

--- services/AuthService.js
+++ services/AuthService.js
@@ -2,17 +2,17 @@ const { errorLogger } = require('./logger')
 const { Users, Providers } = require('../db/models/index.js')
 
 class AuthService {
-	static async findOrCreateOauthProvider(data) {
+	static async findOrCreateOauthProvider({ provider, email, firstName, lastName, providerUserId }) {
 		try {
-			const userData = await Users.findOne({ where: { email: data.email } })
+			const userData = await Users.findOne({ where: { email } })
 			if (userData) {
 				const user = userData.get({ plain: true })
-				const hasAccount = await Providers.findOne({ where: { providerUserId: data.providerUserId } })
-				if (!hasAccount) await this.createOauthAccount(user.id, data.provider, data.providerUserId)
+				const hasAccount = await Providers.findOne({ where: { providerUserId } })
+				if (!hasAccount) await this.createOauthAccount(user.id, provider, providerUserId)
 				return user
 			} else {
-				const newUser = await this.createUser(data.firstName, data.lastName, data.email)
-				await this.createOauthAccount(newUser.dataValues.id, data.provider, data.providerUserId)
+				const newUser = await this.createUser(firstName, lastName, email)
+				await this.createOauthAccount(newUser.dataValues.id, provider, providerUserId)
 				return newUser.get({ plain: true })
 			}
 		} catch (err) {
@@ -20,12 +20,12 @@ class AuthService {
 		}
 	}
 
-	static async createOauthAccount(userID, provider, providerUserId) {
-		await Providers.create({ userId: userID, provider: provider, providerUserId: providerUserId })
+	static async createOauthAccount(userId, provider, providerUserId) {
+		await Providers.create({ userId, provider, providerUserId })
 	}
 
 	static async createUser(firstName, lastName, email) {
-		return await Users.create({ firstName: firstName, lastName: lastName, email: email, password: '' })
+		return await Users.create({ firstName, lastName, email, password: '' })
 	}
 }
 
