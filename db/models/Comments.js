a4f0dc03a72139580ab933be60d374959649926a
Denis Oleschenko <denisoleshenko@gmail.com>
2019-11-08 19:30:58 +0200

    [Comments] - add a model

---
+++ db/models/Comments.js
@@ -0,0 +1,57 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../dbConnection')
+
+class Comments extends Model {}
+
+Comments.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			unique: true,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		content: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		authorId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			field: 'author_id'
+		},
+		articleId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			field: 'article_id'
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			defaultValue: DataTypes.NOW,
+			allowNull: false,
+			field: 'created_at'
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		modelName: 'comments'
+	}
+)
+
+Comments.associate = models => {
+	Comments.belongsTo(models.Users, {
+		as: 'user',
+		foreignKey: 'authorId'
+	})
+}
+
+Comments.associate = models => {
+	Comments.belongsTo(models.Articles, {
+		as: 'article',
+		foreignKey: 'articleId'
+	})
+}
+
+module.exports = Comments
