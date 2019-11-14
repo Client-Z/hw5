87b821186d82c910facfa26f9db679c2959612fc
Denis Oleschenko <denisoleshenko@gmail.com>
2019-11-14 21:13:22 +0200

    fix articles continuation query

--- services/queryHelperService.js
+++ services/queryHelperService.js
@@ -17,7 +17,7 @@ const getArticles = async continuation => {
 		const id = continuation.slice(++beforeID)
 		const timestamp = continuation.slice(0, --beforeID)
 		const publishedAt = moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
-		return await baseQuery({ id: { [Op.lt]: +id }, publishedAt: { [Op.lte]: publishedAt } })
+		return await baseQuery({ id: { [Op.lt]: +id }, publishedAt: { [Op.lt]: publishedAt } }) // e
 	}
 	return await baseQuery()
 }
